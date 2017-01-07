ace2bfc786f4c0d0d412cca6a71399e1ac1caee8
Adam Shumann <ashumann13@gmail.com>
2017-01-07 02:23:30 -0600

    Initial Commit

---
+++ ScreepsRemoteClient/Client.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using Newtonsoft.Json;
+using System.IO;
+using System.Threading;
+using System.Diagnostics;
+
+namespace ScreepsRemoteClient
+{
+    class Client
+    {
+        Socket socket;
+        NetworkStream ns;
+        StreamReader sr;
+        StreamWriter sw;
+        dynamic TerrainData = null;
+        TaskCompletionSource<dynamic> RuntimeDataCompletion;
+        TaskCompletionSource<dynamic> TerrainDataCompletion;
+
+        public Client(string host, int port, string userID) {
+            socket = new Socket(SocketType.Stream, ProtocolType.Tcp);
+            socket.Connect(host,port);
+            Trace.WriteLine(String.Format("Connected {0}",socket.Connected));
+            ns = new NetworkStream(socket, true);
+            UTF8Encoding encoding = new UTF8Encoding(false);
+            sr = new StreamReader(ns, encoding);
+            sw = new StreamWriter(ns, encoding);
+            sw.AutoFlush = true;
+            SendEvent(new RemoteEvent() { name = "auth", data = userID });
+            Thread readThread = new Thread(new ThreadStart(() =>
+            {
+                Task task = ReadLoop();
+                task.Wait();
+            }));
+            readThread.Start();
+        }
+
+        public async Task<dynamic> GetRuntimeData()
+        {
+            RuntimeDataCompletion = new TaskCompletionSource<dynamic>();
+            return await RuntimeDataCompletion.Task;
+        }
+
+        public async Task<dynamic> GetTerrainData()
+        {
+            TerrainDataCompletion = new TaskCompletionSource<dynamic>();
+            if (TerrainData != null)
+            {
+                TerrainDataCompletion.SetResult(TerrainData);
+            }
+            return await TerrainDataCompletion.Task;
+        }
+
+        async Task ReadLoop()
+        {
+            string line;
+            RemoteEvent ev;
+            while (socket.Connected)
+            {
+                line = await sr.ReadLineAsync();
+                if (line == null) continue;
+                ev = JsonConvert.DeserializeObject<RemoteEvent>(line);
+                Trace.WriteLine(String.Format("RecvEvent {0} {1}", ev.name, line));
+                switch (ev.name)
+                {
+                    case "terrainData":
+                        if (TerrainDataCompletion != null)
+                            TerrainDataCompletion.SetResult(ev.data);
+                        TerrainData = ev.data;
+                        break;
+                    case "runtimeData":
+                        if (RuntimeDataCompletion != null)
+                            RuntimeDataCompletion.SetResult(ev.data);
+                        break;
+                }
+            }
+        }
+
+        public void SendTick(ReturnData data)
+        {
+            SendEvent(new RemoteEvent() { name = "tick", data = data });
+        }
+
+        void SendEvent(RemoteEvent e)
+        {
+            string json = Newtonsoft.Json.JsonConvert.SerializeObject(e);
+            Trace.WriteLine(String.Format("SendEvent {0} -- {1}",e.name,json));
+            sw.WriteLine(json);
+        }
+    }
+
+    class RemoteEvent {
+        [JsonProperty(PropertyName="event",Required = Required.Always)]
+        public string name;
+        [JsonProperty(Required = Required.Always)]
+        public dynamic data;
+    }
+
+    class ReturnData
+    {
+        [JsonProperty("console")]
+        public ScreepsConsole Console = new ScreepsConsole();
+        [JsonProperty("intents")]
+        public Dictionary<string, Dictionary<string, Dictionary<string, Intent>>> Intents = new Dictionary<string, Dictionary<string, Dictionary<string, Intent>>>();
+        public void AddIntent(string room, string id, Intent intent)
+        {
+            if (!Intents.ContainsKey(room))
+                Intents.Add(room, new Dictionary<string, Dictionary<string, Intent>> ());
+            if (!Intents[room].ContainsKey(id))
+                Intents[room].Add(id, new Dictionary<string, Intent>());
+            Intents[room][id].Add(intent.IntentType, intent);
+        }
+    }
+
+    class Intent
+    {
+        [JsonIgnore()]
+        public string IntentType;
+    }
+
+    class CreateCreepIntent : Intent
+    {
+        [JsonProperty("name")]
+        public string Name;
+        [JsonProperty("body")]
+        public List<string> Body = new List<string>();
+        [JsonProperty("memory")]
+        public dynamic Memory;
+        public CreateCreepIntent(string name, List<string> body, dynamic memory = null)
+        {
+            IntentType = "createCreep";
+            Name = name;
+            Body = body;
+            Memory = memory;
+        }
+    }
+
+    class MoveIntent : Intent
+    {
+        [JsonProperty("direction")]
+        public int Direction;
+        public MoveIntent(int direction = 0)
+        {
+            IntentType = "move";
+            Direction = direction;
+        }
+    }
+
+    class SayIntent : Intent
+    {
+        [JsonProperty("message")]
+        public string Message;
+        public SayIntent(string message = "")
+        {
+            IntentType = "say";
+            Message = message;
+        }
+    }
+
+    class ScreepsConsole
+    {
+        [JsonProperty("log")]
+        public List<string> Log = new List<string>();
+        [JsonProperty("error")]
+        public List<string> Error = new List<string>();
+        [JsonProperty("result")]
+        public List<string> Result = new List<string>();
+    }
+}
---
+++ ScreepsRemoteClient/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using Newtonsoft.Json.Linq;
+
+namespace ScreepsRemoteClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task main = MainAsync(args);
+            main.Wait();
+        }
+
+        static async Task MainAsync(string[] args)
+        {
+            Console.WriteLine("MainAsync");
+            // Change userid below to your userid obtained from steam client console.
+            Client client = new Client("screeps-test.ags131.ovh",21028,"5779bae444eb975");
+            Console.WriteLine("Awaiting terrain data");
+            dynamic terrainData = await client.GetTerrainData();
+            Console.WriteLine("Looping");
+            while (true)
+            {
+                dynamic runtimeData = await client.GetRuntimeData();
+                dynamic ret = RunCode(terrainData, runtimeData);
+                client.SendTick(ret);
+                Console.WriteLine("Tick {0}", runtimeData.time);
+            }
+        }
+
+        static dynamic RunCode(dynamic terrainData,dynamic runtimeData)
+        {
+            ReturnData ret = new ReturnData();
+            ret.Console.Log.Add(String.Format("Hello from C#! {0}",runtimeData.time));
+            List<dynamic> roomObjects = (from ro in ((JObject)runtimeData.roomObjects).Values<JProperty>()
+                                         select ((JProperty)ro).Value).ToList<dynamic>();
+            string spawnid = (from ro in roomObjects
+                              where ro.type == "spawn"
+                              select ro._id).SingleOrDefault();
+            if(spawnid != null)
+                ret.AddIntent("W2N5", spawnid, new CreateCreepIntent("testing",new List<string>() { "move" }));
+            List<dynamic> creeps = (from ro in roomObjects
+                                    where ro.type == "creep"
+                                    select ro).ToList<dynamic>();
+            Random rand = new Random();
+            foreach(dynamic creep in creeps) {
+                ret.AddIntent((string)creep.room, (string)creep._id, new MoveIntent(rand.Next(0, 9)));
+                ret.AddIntent((string)creep.room, (string)creep._id, new SayIntent("Random!"));
+            }
+            return ret;
+        }
+
+    }
+}
